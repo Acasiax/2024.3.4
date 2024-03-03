81094e8edca216a6f5d3534aaa64a508fb56c1c4
Acasiax <117105267+Acasiax@users.noreply.github.com>
2024-03-04 00:46:21 +0900

    Initial Commit

---
+++ 2024.3.4/_024_3_4App.swift
@@ -0,0 +1,17 @@
+//
+//  _024_3_4App.swift
+//  2024.3.4
+//
+//  Created by 이윤지 on 3/4/24.
+//
+
+import SwiftUI
+
+@main
+struct _024_3_4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
